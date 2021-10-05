d07d5d508718f0d2aa26b0ff2d117dd4fcac0f71
minho0315 <dlalsgh369@naver.com>
2021-10-05 11:28:39 +0900

    엣지와 연결

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const { ApolloServer } = require('apollo-server')
 
-const typeDefs =`
+// 스키마 정의
+const typeDefs = `
 enum PhotoCategory {
     SELFIE
     PORTRAIT
@@ -9,12 +10,22 @@ enum PhotoCategory {
     GRAPHIC
 }
 
+type User {
+    githubLogin: ID!
+    name: String
+    avatar: String
+    postedPhotos: [Photo!]!
+    inPhotos: [Photo!]!
+}
+
 type Photo {
     id: ID!
     url: String!
     name: String!
     description: String
     category: PhotoCategory!
+    postedBy: User!
+    taggedUsers: [User!]!
 }
 
 input PostPhotoInput {
@@ -34,15 +45,61 @@ type Mutation {
 `
 
 var _id = 0
-var photos = []
 
+var tags = [
+    {photoID: '1', userID: 'gPlake'},
+    {photoID: '2', userID: 'sSchmidt'},
+    {photoID: '2', userID: 'mHattrup'},
+    {photoID: '2', userID: 'gPlake'}
+]
+
+var users = [
+    {
+        githubLogin: 'mHattrup',
+        name: 'Mike Hattrup'
+    },
+    {
+        githubLogin: 'gPlake',
+        name: 'Glen Plake'
+    },
+    {
+        githubLogin: 'sSchmidt',
+        name: 'Scot Schmidt'
+    }
+]
+
+var photos = [
+    {
+        id: '1',
+        name: 'Dropping the Heart Chute',
+        description: 'The heart chute is ...',
+        category: 'ACTION',
+        githubLogin: 'gPlake'
+    },
+    {
+        id: '2',
+        name: 'Enjoying the sunshine',
+        category: 'SELFIE',
+        githubLogin: 'sSchmidt'
+    },
+    {
+        id: '3',
+        name: 'Gunbarrel 25',
+        description: '25 laps on gunbarrel today',
+        category: 'LANDSCAPE',
+        githubLogin: 'sSchmidt'
+    }
+]
+
+// 리졸버
 const resolvers = {
     Query: {
-        totalPhotos: () => photos.length
+        totalPhotos: () => photos.length,
+        allPhotos: () => photos
     },
 
     Mutation: {
-        postPhoto (parent, args) {
+        postPhoto(parent, args) {
             var newPhoto = {
                 id: _id++,
                 ...args.input
@@ -53,15 +110,32 @@ const resolvers = {
     },
 
     Photo: {
-        url: parent => `http://yoursite.com/img/${parent.id}.jpg`
+        url: parent => `http://yoursite.com/img/${parent.id}.jpg`,
+        postedBy: parent => {
+            return users.find(u => u.githubLogin === parent.githubLogin)
+        },
+        taggedUsers: parent => tags
+        .filter(tag => tag.photoID === parent.id)
+        .map(tag => tag.userID)
+        .map(userID => users.find(u => u.githubLogin === userID))
+    },
+    User: {
+        postedPhotos: parent => {
+            return photos.filter(p => p.githubUser === parent.githubLogin)
+        },
+        inPhotos: parent => tags
+        .filter(tag => tag.userID === parent.id)
+        .map(tag => tag.photoID)
+        .map(photoID => photos.find(p => p.id === photoID))
     }
 }
 
-const server = new ApolloServer ({
+// 서버
+const server = new ApolloServer({
     typeDefs,
     resolvers
 })
 
 server
-.listen()
-.then(({url}) => console.log(`GraphQL Service running on ${url}`))
+    .listen()
+    .then(({ url }) => console.log(`GraphQL Service running on ${url}`))
