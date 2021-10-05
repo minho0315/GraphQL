5caa14c67bd1863989618dfc8e059c878472154e
minho0315 <dlalsgh369@naver.com>
2021-10-05 10:19:52 +0900

    루트 리졸버, 타입 리졸버

--- index.js
+++ index.js
@@ -1,11 +1,52 @@
+const { ApolloServer } = require('apollo-server')
+
 const typeDefs =`
+type Photo {
+    id: ID!
+    url: String!
+    name: String!
+    description: String
+}
+
 type Query {
     totalPhotos: Int!
+    allPhotos: [Photo!]!
+}
+
+type Mutation {
+    postPhoto(name: String! description: String): Photo!
 }
 `
 
+var _id = 0
+var photos = []
+
 const resolvers = {
     Query: {
-        totalPhotos: () => 42
+        totalPhotos: () => photos.length
+    },
+
+    Mutation: {
+        postPhoto (parent, args) {
+            var newPhoto = {
+                id: _id++,
+                ...args
+            }
+            photos.push(newPhoto)
+            return newPhoto
+        }
+    },
+
+    Photo: {
+        url: parent => `http://yoursite.com/img/${parent.id}.jpg`
     }
 }
+
+const server = new ApolloServer ({
+    typeDefs,
+    resolvers
+})
+
+server
+.listen()
+.then(({url}) => console.log(`GraphQL Service running on ${url}`))
